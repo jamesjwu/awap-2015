33b54f0e0d3c783b4e50b22eac87e2a3d555d9a9
James Wu <jameswu@cmu.edu>
2015-10-17 15:27:27 -0400

    Added vertex ranks

--- src/game/player.py
+++ src/game/player.py
@@ -2,9 +2,10 @@ import networkx as nx
 import random
 from base_player import BasePlayer
 from settings import *
-from collections import deque
+from collections import deque, defaultdict
 
 HAPPY_THRESHOLD = 20
+RANK_THRESHOLD = 3
 
 class Player(BasePlayer):
 
@@ -32,6 +33,8 @@ class Player(BasePlayer):
         self.station_costs = dict([
             (i, INIT_BUILD_COST * (BUILD_FACTOR ** i)) for i in xrange(state.graph.number_of_nodes())
         ])
+
+        self.rank_map = defaultdict(int)
 
         return
 
@@ -82,6 +85,8 @@ class Player(BasePlayer):
                     # if the neighbor is an order, add it to results
                     visited.add(neighbor)
 
+
+
         return sorted(results, key=lambda x: -x[1])
 
     def mark_as_used(self, path, graph):
@@ -92,6 +97,7 @@ class Player(BasePlayer):
         graph = self.state.get_graph()
         order_commands = []
         fulfilled_orders = []
+        print orders
         for (order, val) in orders:
             queue = deque([(order.node, [])])
             visited = set()
@@ -99,6 +105,9 @@ class Player(BasePlayer):
             order_fulfilled = False
             while queue:
                 node, path = queue.popleft()
+                if len(path) < RANK_THRESHOLD:
+                    self.rank_map[node] += RANK_THRESHOLD - len(path)
+
                 if node in self.stations:
                     curr_node = node
                     path.append(node)
@@ -110,14 +119,12 @@ class Player(BasePlayer):
                 filtered_neighbors = [(n, path[:]) for n in neighbors if n not in visited and not graph.edge[node][n]['in_use']]
                 queue.extend(filtered_neighbors)
 
-                print path[::-1]
 
             if order_fulfilled:
                 fulfilled_orders.insert(0, order)
                 order_commands.append(self.send_command(order, path[::-1]))
                 self.mark_as_used(path, graph)
 
-        print orders
         unfulfilled = [(o, v) for (o, v) in orders if o not in fulfilled_orders]
         return order_commands, unfulfilled
 
